2a7c526e8cf9af8087beb704f0aef42f0d8fae99
onikarubi <printuserselect@gmail.com>
2023-12-04 14:55:09 +0900

    Refactor SkillsExperience component to display self introduction as separate paragraphs

--- src/components/SkillsExperience.tsx
+++ src/components/SkillsExperience.tsx
@@ -3,6 +3,14 @@ import { FaCode, FaLaptopCode, FaIndustry, FaGithub, FaUserTie, FaReact, FaTools
 import { SiJavascript, SiPython, SiTypescript, SiDocker, SiGithub, SiCss3, SiHtml5 } from 'react-icons/si';
 
 const SkillsExperience = () => {
+  const prText = `私はフロントエンド開発における豊富な経験と多岐にわたる技術スキルを持つエンジニアとして、ユーザー中心のインタラクティブなウェブアプリケーションの構築に情熱を注いでいます。
+  特に、HTML、CSS、JavaScriptを用いたリッチなUIの開発において、3年以上の経験があります。ReactとNext.jsを駆使し、現代的で応答性の高いウェブインターフェースを設計・実装してきました。
+  さらに、SassやTypeScriptなどの先進的なフロントエンド技術も活用して、より効率的で読みやすいコードを書くことに重点を置いています。これらのスキルは、プロジェクトの要件を正確に捉え、ユーザーに最適なエクスペリエンスを提供するために不可欠です。
+  加えて、バックエンドとの連携においても、PythonやNode.jsの知識を生かし、フルスタック開発のプロジェクトにも積極的に参加してきました。AWSの基礎知識とDockerを用いたアプリケーションのデプロイメントにも携わり、開発から運用までの幅広い工程に理解を持っています。
+  チームプロジェクトにおいては、GitHubを活用した効果的なバージョン管理とコラボレーションを通じて、チームメンバーと密に連携し、プロジェクトを成功に導いてきました。常に新しい技術の習得にも積極的であり、最新のトレンドを取り入れた開発を行うことで、プロジェクトに革新的なアプローチをもたらしています。`;
+
+  const paragraphs = prText.split('。').filter(p => p).map((p, index) => <p key={index}>{p}。</p>);
+
   return (
     <div className="bg-white shadow-xl rounded-lg p-8 mt-8">
       <h2 className="text-2xl font-bold text-gray-800 mb-6 flex items-center gap-2">
@@ -64,10 +72,14 @@ const SkillsExperience = () => {
           <strong>ポートフォリオ・GitHub:</strong>
           <a href="https://github.com/onikarubi?tab=repositories" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800 ml-2">GitHub</a>
         </p>
-        <p className="flex items-center gap-2 col-span-1 md:col-span-2">
-          <FaUserTie className="text-red-600" />
-          <strong>自己PR:</strong> ユーザー中心のインタラクティブなウェブアプリケーションの構築に情熱を注ぐエンジニア。HTML、CSS、JavaScriptに3年以上の経験。
-        </p>
+        <div className="mt-6">
+          <h3 className="text-2xl font-bold text-gray-800 mb-4 flex items-center gap-2">
+            <FaUserTie className="text-red-600" /> 自己PR
+          </h3>
+          <div className="text-gray-700 text-base leading-relaxed">
+            {paragraphs}
+          </div>
+        </div>
       </div>
     </div>
   );
